8683da74e1cdee061ed7e9a1f484c60a960eb503
Tshedza Mathada <ST10131676@rcconnect.edu.za>
2023-10-13 13:39:36 +0000

    I added a functionality that allows user to unlike.

--- app/src/main/java/com/example/ebird/BirdAdapter.java
+++ app/src/main/java/com/example/ebird/BirdAdapter.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,10 +14,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.ebird.Models.NearbyVariables;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+
 
 public class BirdAdapter extends FirestoreRecyclerAdapter<NearbyVariables, BirdAdapter.BirdViewHolder> {
 
     Context context;
+
 
 
     public BirdAdapter(@NonNull FirestoreRecyclerOptions<NearbyVariables> options, Context context) {
@@ -40,12 +47,32 @@ public class BirdAdapter extends FirestoreRecyclerAdapter<NearbyVariables, BirdA
 
     class BirdViewHolder extends RecyclerView.ViewHolder{
         TextView sci_name, com_name, loc_name;
+        ImageView unlikebtn;
 
         public BirdViewHolder(@NonNull View itemView) {
             super(itemView);
             sci_name = itemView.findViewById(R.id.fav_specie);
             com_name = itemView.findViewById(R.id.fav_birdname);
             loc_name = itemView.findViewById(R.id.fav_birdLocation);
+            unlikebtn = itemView.findViewById(R.id.iLFavorite);
+
+            unlikebtn.setOnClickListener((v) -> deleteFromFirebase());
+        }
+        void deleteFromFirebase(){
+            DocumentReference documentReference;
+            documentReference = Utility.getCollectionReferenceForData().document();
+
+            documentReference.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+
+                    if (task.isSuccessful()) {
+                        Toast.makeText(context, "unsaved!", Toast.LENGTH_SHORT).show();
+                    }else {
+                        Toast.makeText(context, "still saved!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         }
     }
 }
